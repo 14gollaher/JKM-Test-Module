be646b0bb8b7b4546ca1499f9fca3df204cd7104
Unknown <kyle.paxton@jacks.sdstate.edu>
2018-02-02 20:47:44 -0600

    Added Results Page

--- TestProjects/ProofOfConcept/ProofOfConcept/tango/urls.py
+++ TestProjects/ProofOfConcept/ProofOfConcept/tango/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 import tango.views
 
 urlpatterns = [
+    url(r'^results', tango.views.viewResults, name = "results"),
     url(r'^(?P<viewToTest>\w+|)', tango.views.viewTest, name = "viewTest")
 ]
-
--- TestProjects/ProofOfConcept/ProofOfConcept/tango/views.py
+++ TestProjects/ProofOfConcept/ProofOfConcept/tango/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-
 def viewTest(request, viewToTest):
     if viewToTest is None: viewToTest = ''
     return render(
         request,
-        'tango/index.html',
+        'tango/testing.html',
         {
             'viewToTest': viewToTest
         }
+    )
+
+def viewResults(request):
+    return render(
+        request,
+        'tango/results.html'
     )
