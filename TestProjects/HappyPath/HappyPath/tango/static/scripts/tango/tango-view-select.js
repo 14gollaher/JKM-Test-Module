f7607c45fb008fdb06b9992932c6057a0f0e457f
Unknown <kyle.paxton@jacks.sdstate.edu>
2018-03-20 16:24:43 -0500

    fixed select view and modal appearance

--- TestProjects/HappyPath/HappyPath/tango/static/scripts/tango/tango-view-select.js
+++ TestProjects/HappyPath/HappyPath/tango/static/scripts/tango/tango-view-select.js
@@ -8,22 +8,40 @@ function updateViewList() {
         return;
     }
 
-    var html = '<table class="uk-table uk-table-striped">';
-    html += '<tr>';
-    html += '<th>View Name</th>';
-    html += '</tr>';
+    var html = '';
     for (i in views) {
-        html += '<tr>';
-        html += '<td><a href=' + views[i]['name'] + '>';
-
-        html += views[i]['name']
-        html += '<iframe src="' + baseUrl + '/' + views[i]['name'] + '"></iframe>';
-
-        html += '</a></td>';
-        html += '</tr>';
+        html += '<a href=' + views[i]['name'] + '>';
+        html += '<div class="uk-card-default uk-card-hover uk-margin-medium-top uk-margin-medium-left uk-margin-medium-bottom">';
+        html += '<h3 style="color: black; font-weight: bold;">' + views[i]['name'] + '</h3>';
+        html += '<iframe class="uk-border-rounded" src="' + baseUrl + '/' + views[i]['name'] + '" style="width:90%; height:500px;"></iframe>';
+        html += '</div>';
+        html += '</a>';
     }
+    for (i in views) {
+        html += '<a href=' + views[i]['name'] + '>';
+        html += '<div class="uk-card-default uk-card-hover uk-margin-medium-top uk-margin-medium-left uk-margin-medium-bottom">';
+        html += '<h3 style="color: black; font-weight: bold;">' + views[i]['name'] + '</h3>';
+        html += '<iframe class="uk-border-rounded" src="' + baseUrl + '/' + views[i]['name'] + '" style="width:90%; height:500px;"></iframe>';
+        html += '</div>';
+        html += '</a>';
+    }
+    for (i in views) {
+        html += '<a href=' + views[i]['name'] + '>';
+        html += '<div class="uk-card-default uk-card-hover uk-margin-medium-top uk-margin-medium-left uk-margin-medium-bottom">';
+        html += '<h3 style="color: black; font-weight: bold;">' + views[i]['name'] + '</h3>';
+        html += '<iframe class="uk-border-rounded" src="' + baseUrl + '/' + views[i]['name'] + '" style="width:90%; height:500px;"></iframe>';
+        html += '</div>';
+        html += '</a>';
+    }
+    for (i in views) {
+        html += '<a href=' + views[i]['name'] + '>';
+        html += '<div class="uk-card-default uk-card-hover uk-margin-medium-top uk-margin-medium-left uk-margin-medium-bottom">';
+        html += '<h3 style="color: black; font-weight: bold;">' + views[i]['name'] + '</h3>';
+        html += '<iframe class="uk-border-rounded" src="' + baseUrl + '/' + views[i]['name'] + '" style="width:90%; height:500px;"></iframe>';
+        html += '</div>';
+        html += '</a>';
+    }
+    
 
-
-    html += '</table>';
     document.getElementById('view-table').innerHTML = html;
 }
