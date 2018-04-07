30e7ecdbae8fd1aeaf4727759d5e6a5918f0f511
Unknown <kyle.paxton@jacks.sdstate.edu>
2018-04-07 00:07:35 -0500

    began implementing new test app

--- TestProjects/HappyPath/HappyPath/HappyPath/settings.py
+++ TestProjects/HappyPath/HappyPath/HappyPath/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 STATIC_URL = '/static/'
 
--- TestProjects/HappyPath/HappyPath/app/forms.py
+++ TestProjects/HappyPath/HappyPath/app/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
+
 class HappyPathForm(forms.Form):
     firstName = forms.CharField(label='First name', max_length=50)
     age = forms.IntegerField(label='Age', min_value = 10, max_value=100)
@@ -10,14 +14,25 @@ class SadPathForm(forms.Form):
     dog = forms.IntegerField(label='Age', min_value = 10, max_value=100)
 
 class SuperForm(forms.Form):
+    helper = FormHelper()
+    helper.label_class = 'col-md-2 col-md-offset-4 alignRight'
+    helper.field_class = 'col-md-2'
     firstName = forms.CharField(label='First name', max_length=50, min_length=2, initial='Your name', required=False )
     dateStuff = forms.DateField(label='thedate')
     decimalStuff = forms.DecimalField(label='decimalstuff', max_value = 4224.2, min_value=23, max_digits=23, decimal_places=2)
     floatStuff = forms.FloatField(label='floatstuff', max_value=1223, min_value=12)
     genericIpAddress = forms.GenericIPAddressField(label='ipaddress', protocol='ipv4')
     TimeFields = forms.TimeField(label='timesstuff', input_formats='%H:%M:%S')
-    URLFields = forms.URLField(label='urlstuff', max_length=123, min_length=12)
-
+    URLFields = forms.URLField(label='urlstuff', max_length=123, min_length=12)    
+    helper.layout = Layout(
+        Field('firstName'),
+        Field('dateStuff'),
+        Field('decimalStuff'),
+        Field('floatStuff'),
+        Field('genericIpAddress'),
+        Field('TimeFields'),
+        Field('URLFields'),
+    )
 class ClassForm(forms.Form):
     firstName = forms.CharField(label='First name', max_length=20, min_length=3, initial='Your name', required=False )
     dateStuff = forms.DateField(label='thedate')
