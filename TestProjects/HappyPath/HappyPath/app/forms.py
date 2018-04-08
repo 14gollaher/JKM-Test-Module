60351a20faa423a61380379108454eebf546ac19
Unknown <kyle.paxton@jacks.sdstate.edu>
2018-04-07 22:04:06 -0500

    fixed up the test app

--- TestProjects/HappyPath/HappyPath/app/forms.py
+++ TestProjects/HappyPath/HappyPath/app/forms.py
@@ -9,14 +9,24 @@ class HappyPathForm(forms.Form):
     age = forms.IntegerField(label='Age', min_value = 10, max_value=100)
 
 class SadPathForm(forms.Form):
+    helper = FormHelper()
+    helper.label_class = 'col-sm-2 col-xs-5 col-sm-offset-4'
+    helper.field_class = 'col-sm-2 col-xs-5'
     firstName = forms.CharField(label='First name', max_length=50)
     age = forms.IntegerField(label='Age', min_value = 10, max_value=100)
     dog = forms.IntegerField(label='Age', min_value = 10, max_value=100)
-
+    helper.layout = Layout(
+        Field('firstName'),
+        Field('age'),
+        Field('dog'),
+        FormActions(
+            Submit('save_changes', 'Save changes', css_class="btn-primary"),
+        )
+    )
 class SuperForm(forms.Form):
     helper = FormHelper()
-    helper.label_class = 'col-md-2 col-md-offset-4 alignRight'
-    helper.field_class = 'col-md-2'
+    helper.label_class = 'col-sm-2 col-xs-5 col-sm-offset-4'
+    helper.field_class = 'col-sm-2 col-xs-5'
     firstName = forms.CharField(label='First name', max_length=50, min_length=2, initial='Your name', required=False )
     dateStuff = forms.DateField(label='thedate')
     decimalStuff = forms.DecimalField(label='decimalstuff', max_value = 4224.2, min_value=23, max_digits=23, decimal_places=2)
@@ -32,8 +42,14 @@ class SuperForm(forms.Form):
         Field('genericIpAddress'),
         Field('TimeFields'),
         Field('URLFields'),
+        FormActions(
+            Submit('save_changes', 'Save changes', css_class="btn-primary"),
+        )
     )
 class ClassForm(forms.Form):
+    helper = FormHelper()
+    helper.label_class = 'col-sm-2 col-xs-5 col-sm-offset-4'
+    helper.field_class = 'col-sm-2 col-xs-5'
     firstName = forms.CharField(label='First name', max_length=20, min_length=3, initial='Your name', required=False )
     dateStuff = forms.DateField(label='thedate')
     decimalStuff = forms.DecimalField(label='decimalstuff', max_value=4224.2, min_value=23, max_digits=23, decimal_places=2)
@@ -41,5 +57,16 @@ class ClassForm(forms.Form):
     genericIpAddress = forms.GenericIPAddressField(label='ipaddress', protocol='ipv4')
     TimeFields = forms.TimeField(label='timesstuff', input_formats='%H:%M:%S')
     URLFields = forms.URLField(label='urlstuff', max_length=123, min_length=12)
-    affanField = forms.URLField(label='urlstuff', max_length=123, min_length=12)
+    helper.layout = Layout(
+        Field('firstName'),
+        Field('dateStuff'),
+        Field('decimalStuff'),
+        Field('floatStuff'),
+        Field('genericIpAddress'),
+        Field('TimeFields'),
+        Field('URLFields'),
+        FormActions(
+            Submit('save_changes', 'Save changes', css_class="btn-primary"),
+        )
+    )
 
